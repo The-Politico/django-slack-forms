613a5c7c3411926621f79e1c13cef930a68f38ef
Andrew Briz <briz.andrew@gmail.com>
2018-12-13 17:35:44 -0500

    Forgot an endpoint.

--- slackforms/urls.py
+++ slackforms/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Home, Callback
+from .views import Home, Callback, Message
 
 urlpatterns = [
     path("", Home.as_view(), name="slackforms-home"),
     path("callback/", Callback.as_view(), name="slackforms-callback"),
+    path("message/", Callback.as_view(), name="slackforms-message"),
 ]
